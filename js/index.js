0f610d4db1ff52c8064b3ae720dec32f1b836f69
scottelsy <40515832+scottyelsy@users.noreply.github.com>
2019-12-24 13:16:27 +0000

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,25 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+
+  /*** Countdown Timer */
+  $("#days")
+  .countdown("2020/01/01", function (event) {
+      $(this).html(event.strftime('%D <em>days</em>'))
+  });
+
+  $("#hours")
+  .countdown("2020/01/01", function (event) {
+      $(this).html(event.strftime('%H <em>hours</em>'))
+  });
+
+  $("#minutes")
+  .countdown("2020/01/01", function (event) {
+      $(this).html(event.strftime('%M <em>minutes</em>'))
+  });
+
+  $("#seconds")
+  .countdown("2020/01/01", function (event) {
+      $(this).html(event.strftime('%S <em>seconds</em>'))
+  });
